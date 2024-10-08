d7b3506dc882cd6303f218b08e3552c167341a32
Gabriel Toffanetto <gabrieltoffanetto@gmail.com>
2024-10-08 13:28:11 -0300

    Erasing build files

--- micro-ros/build/command_stm32_leds/CMakeFiles/command_stm32_leds_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/command_stm32_leds_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/command_stm32_leds_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
