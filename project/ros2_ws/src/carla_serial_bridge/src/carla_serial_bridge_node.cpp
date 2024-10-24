7778e9b4418e7b1dde6e9e261a3fa3c6a71230d3
Gabriel Toffanetto <gabrieltoffanetto@gmail.com>
2024-10-24 19:48:01 -0300

    Carla serial bridge with control pub and status sub

--- project/ros2_ws/src/carla_serial_bridge/src/carla_serial_bridge_node.cpp
+++ project/ros2_ws/src/carla_serial_bridge/src/carla_serial_bridge_node.cpp
@@ -1,21 +1,28 @@
+
 #include <chrono>
 #include <functional>
 #include <memory>
 
-#include "rclcpp/rclcpp.hpp"
-//#include "sensor_msgs/msg/point_cloud2.hpp"
+#include <cstring>
 
-#include <carla_serial_bridge/serial_com.hpp>
+#include "rclcpp/rclcpp.hpp"
+//#include "sensor_rx_msgs/rx_msg/point_cloud2.hpp"
+
+#include "carla_serial_bridge/serial_com.hpp"
+#include "carla_msgs/msg/carla_ego_vehicle_control.hpp"
+#include "carla_msgs/msg/carla_ego_vehicle_status.hpp"
 
 #define PORT "/dev/ttyACM0"
 #define BAUDRATE B115200
 
-using namespace std::chrono_literals;
-
 typedef union{
   float f;
-  __uint8_t bytes[4];
+  char bytes[4];
 } float_bytes;
+
+using namespace std::chrono_literals;
+
+using std::placeholders::_1;
 
 class CarlaSerialBridge : public rclcpp::Node{
   public:
@@ -26,53 +33,222 @@ class CarlaSerialBridge : public rclcpp::Node{
       // TODO baudrate = this->get_parameter("baudware");
       // TODO port_name = this->get_parameter("port_name");
 
-      timer_ = this->create_wall_timer(1000ms,
-                                       std::bind(&CarlaSerialBridge::timer_callback, this));
-      ss_state = 0;
+      vehicle_control_pub_ = this->create_publisher<carla_msgs::msg::CarlaEgoVehicleControl>("/carla/hero/vehicle_control_cmd", 10); // This 10 is QoS?
+
+      vehicle_status_sub_ = this->create_subscription<carla_msgs::msg::CarlaEgoVehicleStatus>("/carla/hero/vehicle_status", 
+                                                                                              10, std::bind(&CarlaSerialBridge::vehicle_status_sub_callback, this, _1));
+
+      timer_ = this->create_wall_timer(1000ms, std::bind(&CarlaSerialBridge::timer_callback, this));
+      sm_state = 0;
       
     }
 
   private:
 
+    void vehicle_status_sub_callback(const carla_msgs::msg::CarlaEgoVehicleStatus::SharedPtr msg) const{
+
+    }
+
+    void publish_to_carla(){
+    /**
+     * Empacotar:
+     *  throttle_rx.f
+     *  steering_rx.f
+     *  brake_rx.f
+     *  (int)hand_brake_rx
+     *  (int)manual_shift_rx
+     *  (int)reverse_rx
+     *  (int)gear_rx
+     * 
+     * Tipo da msg: CarlaEgoVehicleControl.msg
+     * carla_msgs::msg::CarlaEgoVehicleControl
+     */
+
+        auto vehicle_control_msg_ = carla_msgs::msg::CarlaEgoVehicleControl();
+
+        vehicle_control_msg_.throttle = throttle_rx.f;
+        vehicle_control_msg_.steer = steering_rx.f;
+        vehicle_control_msg_.brake = brake_rx.f;
+        vehicle_control_msg_.hand_brake = (bool) hand_brake_rx;
+        vehicle_control_msg_.reverse = (bool) reverse_rx;
+        vehicle_control_msg_.manual_gear_shift = (bool) manual_shift_rx;
+        vehicle_control_msg_.gear = (__uint32_t) gear_rx;
+
+        // TODO vehicle_control_msg_->header
+        // ! stamp pending
+
+        vehicle_control_pub_->publish(vehicle_control_msg_);
+    }
+
     void timer_callback(){
-      unsigned char * rx_msg;
+      char * rx_msg;
       rx_msg = serial_com_link.readSerialPort();
 
-      for(int i = 0; i < sizeof(rx_msg); i++){
-        if(rx_msg[i])
-      }
+     for(int i = 0; i < (int)strlen(rx_msg); i++){
+
+        switch (sm_state){
+            case 0:
+                if('#' == rx_msg[i]){
+                    sm_state = 1;                 
+                }
+                break;
+            
+            case 1:
+                switch (rx_msg[i]){
+                    case 'T':
+                        sm_state = 20;                 
+                        break;
+
+                    case 'S':
+                        sm_state = 30;                 
+                        break;
+
+                    case 'B':
+                        sm_state = 40;                 
+                        break;
+
+                    case 'H':
+                        sm_state = 50;                 
+                        break;
+
+                    case 'M':
+                        sm_state = 60;                 
+                        break;
+
+                    case 'R':
+                        sm_state = 70;                 
+                        break;
+
+                    case 'G':
+                        sm_state = 80;                                     
+                        break;
+
+                    case '$':
+                        sm_state = 0;   
+                        this->publish_to_carla();              
+                        break;
+                    
+                    default:
+                        break;
+                }
+                break;        
+            
+            case 20:
+                throttle_rx.bytes[0] = rx_msg[i];
+                sm_state = 21;                 
+                break;        
+            
+            case 21:
+                throttle_rx.bytes[1] = rx_msg[i];
+                sm_state = 22;                 
+                break;        
+            
+            case 22:
+                throttle_rx.bytes[2] = rx_msg[i];
+                sm_state = 23;                 
+                break;        
+            
+            case 23:
+                throttle_rx.bytes[3] = rx_msg[i];
+                sm_state = 1;                 
+                break;        
+            
+            case 30:
+                steering_rx.bytes[0] = rx_msg[i];
+                sm_state = 31;                 
+                break;        
+            
+            case 31:
+                steering_rx.bytes[1] = rx_msg[i];
+                sm_state = 32;                 
+                break;
+            
+            
+            case 32:
+                steering_rx.bytes[2] = rx_msg[i];
+                sm_state = 33;                 
+                break;        
+            
+            case 33:
+                steering_rx.bytes[3] = rx_msg[i];
+                sm_state = 1;                 
+                break;        
+            
+            case 40:
+                brake_rx.bytes[0] = rx_msg[i];
+                sm_state = 41;                 
+                break;        
+            
+            case 41:
+                brake_rx.bytes[1] = rx_msg[i];
+                sm_state = 42;                 
+                break;        
+            
+            case 42:
+                brake_rx.bytes[2] = rx_msg[i];
+                sm_state = 43;                 
+                break;        
+            
+            case 43:
+                brake_rx.bytes[3] = rx_msg[i];
+                sm_state = 1;                 
+                break;        
+            
+            case 50:
+                hand_brake_rx = rx_msg[i];
+                sm_state = 1;                             
+                break;        
+            
+            case 60:
+                manual_shift_rx = rx_msg[i];
+                sm_state = 1;                            
+                break;        
+            
+            case 70:
+                reverse_rx = rx_msg[i];
+                sm_state = 1;                             
+                break;        
+            
+            case 80:
+                gear_rx = rx_msg[i];
+                sm_state = 1;                             
+                break;
+            
+            default:
+                break;
+        }
+    }
 
       // Process rx_msg
 
       // Publish to CARLA if available
 
-      //char * tx_msg;
+      //char * tx_rx_msg;
 
 
-      // Process topics to tx_msg
-      //serial_com_link.writeSerialPort(tx_msg);
+      // Process topics to tx_rx_msg
+      //serial_com_link.writeSerialPort(tx_rx_msg);
       
     }
 
     rclcpp::TimerBase::SharedPtr timer_;
 
-    rclcpp::Publisher<type>::SharedPtr ackerman_cmd_pub_;
-    rclcpp::Publisher<type>::SharedPtr publisher_;
+    rclcpp::Publisher<carla_msgs::msg::CarlaEgoVehicleControl>::SharedPtr vehicle_control_pub_;
 
-    rclcpp::Subscription<type>::SharedPtr subscription_;
+    rclcpp::Subscription<carla_msgs::msg::CarlaEgoVehicleStatus>::SharedPtr vehicle_status_sub_;
 
-    __u_int loop_rate;
-    __uint16_t baudrate;
+    __uint32_t loop_rate;
+    __uint32_t baudrate;
     char * port_name;
 
-    __uint8_t ss_state;
-    float_bytes trottle_rx;
+    char sm_state;
+    float_bytes throttle_rx;
     float_bytes steering_rx;
     float_bytes brake_rx;
-    __uint8_t hand_brake_rx;
-    __uint8_t manual_shift_rx;
-    __uint8_t reverse_rx;
-    __uint8_t gear_rx;
+    char hand_brake_rx;
+    char manual_shift_rx;
+    char reverse_rx;
+    char gear_rx;
 
     
 
--- project/ros2_ws/src/carla_serial_bridge/src/ss.cpp
+++ project/ros2_ws/src/carla_serial_bridge/src/ss.cpp
@@ -6,159 +6,165 @@ using namespace std;
 
 typedef union{
   float f;
-  uint8_t bytes[4];
+  char bytes[4];
 } float_bytes;
 
 int main(){
 
-    uint8_t ss_state = 0;
+    int ss_state = 0;
     float_bytes trottle_rx;
     float_bytes steering_rx;
     float_bytes brake_rx;
-    uint8_t hand_brake_rx;
-    uint8_t manual_shift_rx;
-    uint8_t reverse_rx;
-    uint8_t gear_rx;
+    char hand_brake_rx;
+    char manual_shift_rx;
+    char reverse_rx;
+    char gear_rx;
 
     // #Tf[0:3]Sf[0:3]Bf[0:3]HbRbGiMb$
-    char msg[100];
-    sprintf(msg, "#T%fS%fB%fH%dR%dG%dM%d$",11.11,22.22,33.33,1,1,23,1);
+    char * msg;
+
+    float_bytes t;
+    t.f = 11.11;
+
+    float_bytes s;
+    s.f = 22.11;
+
+    float_bytes b;
+    b.f = 33.11;
+
+    sprintf(msg, "#T%c%c%c%cS%c%c%c%cB%c%c%c%cH%cR%cG%cM%c$",
+            t.bytes[0],t.bytes[1],t.bytes[2],t.bytes[3],
+            s.bytes[0],s.bytes[1],s.bytes[2],s.bytes[3],
+            b.bytes[0],b.bytes[1],b.bytes[2],b.bytes[3],
+            1,1,23,1);
 
     for(int i = 0; i<strlen(msg); i++){
-        cout << i << endl;
+
         switch (ss_state){
         case 0:
-            if('#' == (uint8_t) msg[i]){
-                ss_state = 1;
+            if('#' == msg[i]){
+                ss_state = 1;                 
             }
             break;
         
         case 1:
-            switch ((uint8_t) msg[i]){
+            switch (msg[i]){
                 case 'T':
-                    ss_state = 20;
+                    ss_state = 20;                 
                     break;
+
                 case 'S':
-                    ss_state = 30;
+                    ss_state = 30;                 
                     break;
+
                 case 'B':
-                    ss_state = 40;
+                    ss_state = 40;                 
                     break;
+
                 case 'H':
-                    ss_state = 50;
+                    ss_state = 50;                 
                     break;
+
                 case 'M':
-                    ss_state = 60;
+                    ss_state = 60;                 
                     break;
+
                 case 'R':
-                    ss_state = 70;
+                    ss_state = 70;                 
                     break;
+
                 case 'G':
-                    ss_state = 80;
+                    ss_state = 80;                                     
                     break;
+
                 case '$':
-                    ss_state = 0;
+                    ss_state = 0;          
                     break;
                 
                 default:
                     break;
             }
-            break;
-        
+            break;        
         
         case 20:
-            trottle_rx.bytes[0] = (uint8_t) msg[i];
-            ss_state = 21;
-            break;
-        
+            trottle_rx.bytes[0] = msg[i];
+            ss_state = 21;                 
+            break;        
         
         case 21:
-            trottle_rx.bytes[1] = (uint8_t) msg[i];
-            ss_state = 22;
-            break;
-        
+            trottle_rx.bytes[1] = msg[i];
+            ss_state = 22;                 
+            break;        
         
         case 22:
-            trottle_rx.bytes[2] = (uint8_t) msg[i];
-            ss_state = 23;
-            break;
-        
+            trottle_rx.bytes[2] = msg[i];
+            ss_state = 23;                 
+            break;        
         
         case 23:
-            trottle_rx.bytes[3] = (uint8_t) msg[i];
-            ss_state = 1;
-            break;
-        
+            trottle_rx.bytes[3] = msg[i];
+            ss_state = 1;                 
+            break;        
         
         case 30:
-            steering_rx.bytes[0] = (uint8_t) msg[i];
-            ss_state = 31;
-            break;
-        
+            steering_rx.bytes[0] = msg[i];
+            ss_state = 31;                 
+            break;        
         
         case 31:
-            steering_rx.bytes[1] = (uint8_t) msg[i];
-            ss_state = 32;
+            steering_rx.bytes[1] = msg[i];
+            ss_state = 32;                 
             break;
         
         
         case 32:
-            steering_rx.bytes[2] = (uint8_t) msg[i];
-            ss_state = 33;
-            break;
-        
+            steering_rx.bytes[2] = msg[i];
+            ss_state = 33;                 
+            break;        
         
         case 33:
-            steering_rx.bytes[3] = (uint8_t) msg[i];
-            ss_state = 1;
-            break;
-        
+            steering_rx.bytes[3] = msg[i];
+            ss_state = 1;                 
+            break;        
         
         case 40:
-            brake_rx.bytes[0] = (uint8_t) msg[i];
-            ss_state = 41;
-            break;
-        
+            brake_rx.bytes[0] = msg[i];
+            ss_state = 41;                 
+            break;        
         
         case 41:
-            brake_rx.bytes[1] = (uint8_t) msg[i];
-            ss_state = 42;
-            break;
-        
+            brake_rx.bytes[1] = msg[i];
+            ss_state = 42;                 
+            break;        
         
         case 42:
-            brake_rx.bytes[2] = (uint8_t) msg[i];
-            ss_state = 43;
-            break;
-        
+            brake_rx.bytes[2] = msg[i];
+            ss_state = 43;                 
+            break;        
         
         case 43:
-            brake_rx.bytes[3] = (uint8_t) msg[i];
-            ss_state = 1;
-            break;
-        
+            brake_rx.bytes[3] = msg[i];
+            ss_state = 1;                 
+            break;        
         
         case 50:
-            hand_brake_rx = (uint8_t) msg[i];
-            ss_state = 1;            
-            break;
-        
+            hand_brake_rx = msg[i];
+            ss_state = 1;                             
+            break;        
         
         case 60:
-            manual_shift_rx = (uint8_t) msg[i];
-            ss_state = 1;            
-            break;
-        
+            manual_shift_rx = msg[i];
+            ss_state = 1;                            
+            break;        
         
         case 70:
-            reverse_rx = (uint8_t) msg[i];
-            ss_state = 1;            
-            break;
-        
+            reverse_rx = msg[i];
+            ss_state = 1;                             
+            break;        
         
         case 80:
-            gear_rx = (uint8_t) msg[i];
-            ss_state = 1;            
+            gear_rx = msg[i];
+            ss_state = 1;                             
             break;
         
         default:
@@ -166,8 +172,14 @@ int main(){
         }
     }
 
-    cout << msg << endl
-         << trottle_rx.f << endl;
+    cout << endl << msg << endl
+         << trottle_rx.f << endl
+         << brake_rx.f << endl
+         << steering_rx.f << endl
+         << (int)hand_brake_rx << endl
+         << (int)gear_rx << endl
+         << (int)reverse_rx << endl
+         << (int)manual_shift_rx << endl;
 
 
 }
